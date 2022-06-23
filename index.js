f2e09a9e24ea0bc76a418dd24ccff02ee36bc030
Shane Mc Cormack <shanemcc@gmail.com>
2022-06-23 01:06:34 +0100

    Add GET support.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import http from 'http'
+import { default as nodeurl, URLSearchParams } from 'url'
 
 import scrape from './src/scraper.js'
 
@@ -23,12 +24,52 @@ const semaphore = new Semaphore(maxConcurrentRequests);
 
 var scrapeID = 0;
 
+const handleScrapeRequest = async function(res, url) {
+    const thisScrapeID = `req-${scrapeID++}`;
+    await semaphore.runExclusive(async () => {
+        console.log(`\t{${thisScrapeID}} Got lock`);
+        console.log(`\t{${thisScrapeID}} Scraping: ${url}`);
+        const result = await scrape(url, thisScrapeID);
+        var statusCode; 
+
+        if (result['info']['error'] === undefined) {
+            console.log(`\t\t{${thisScrapeID}} Success.`);
+            statusCode = 200;
+        } else {
+            console.log(`\t\t{${thisScrapeID}} Failed. (${result['info']['error']})`);
+            statusCode = 500;
+        }
+
+        result['info']['scrape-id'] = thisScrapeID;
+        res.writeHead(statusCode, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(result, null, 2));
+    });
+}
+
+const handleScrapeError = async function(res, message, extra, code = 400) {
+    console.log(`\tError ${message} ${extra}`);
+    res.writeHead(code, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ 'info': { 'version': '2', 'code': code, 'error': message } }, null, 2));
+}
+
 const scrapeHandler = async function (req, res) {
     if (req.method === "GET") {
-        console.log(`\tGET - Ignoring.`);
+        console.log(`\tGET - Handling.`);
 
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ 'info': { 'version': '2', 'message': 'There might be something here.' } }, null, 2));
+        const searchParams = new URLSearchParams(nodeurl.parse(req.url).search);
+        const url = searchParams.get('url');
+        const key = searchParams.get('key');
+
+        if (key !== null && key === validKEY) {
+            if (url !== null) {
+                await handleScrapeRequest(res, url);
+            } else {
+                await handleScrapeError(res, 'Invalid or missing URL');
+            }
+        } else {
+            await handleScrapeError(res, 'Invalid or missing API Key', key);
+        }
+        
     } else if (req.method === "POST") {
         console.log(`\tPOST - Handling.`);
 
@@ -39,42 +80,23 @@ const scrapeHandler = async function (req, res) {
                 const bodyjson = JSON.parse(body);
 
                 if (bodyjson['url'] !== undefined) {
-                    const thisScrapeID = `req-${scrapeID++}`;
-                    await semaphore.runExclusive(async () => {
-                        console.log(`\t{${thisScrapeID}} Got lock`);
-                        console.log(`\t{${thisScrapeID}} Scraping: ${bodyjson['url']}`);
-                        const result = await scrape(bodyjson['url'], thisScrapeID);
-                        var statusCode; 
-
-                        if (result['info']['error'] === undefined) {
-                            console.log(`\t\t{${thisScrapeID}} Success.`);
-                            statusCode = 200;
-                        } else {
-                            console.log(`\t\t{${thisScrapeID}} Failed. (${result['info']['error']})`);
-                            statusCode = 500;
-                        }
-
-                        result['info']['scrape-id'] = thisScrapeID;
-                        res.writeHead(statusCode, { "Content-Type": "application/json" });
-                        res.end(JSON.stringify(result, null, 2));
-                    });
+                    await handleScrapeRequest(res, bodyjson['url']);
+                } else {
+                    await handleScrapeError(res, 'Invalid or missing URL');
                 }
             });
         } else {
-            console.log(`\tInvalid or missing API Key: ${req.headers['x-rapidapi-key']}`);
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ 'info': { 'version': '2', 'error': 'Invalid or missing API Key' } }, null, 2));
+            await handleScrapeError(res, 'Invalid or missing API Key', req.headers['x-rapidapi-key']);
         }
     }
 }
 
 const requestListener = async function (req, res) {
-    req.on('error', err => {
+    req.on('error', async err => {
         console.err('Request error:');
         console.error(err);
 
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ 'info': { 'version': '2', 'error': 'Bad Request' } }, null, 2));
+        await handleScrapeError(res, 'Bad Request');
         return;
     });
 
@@ -83,16 +105,17 @@ const requestListener = async function (req, res) {
         console.error(err);
     });
 
-    var remoteIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
-    console.log('Request from ' + remoteIP + ' for: ' + req.url);
+    const remoteIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+    const urlbits = nodeurl.parse(req.url);
 
-    if (req.url.toLowerCase() == '/scrape') {
+    console.log('Request from ' + remoteIP + ' for: ' + urlbits.pathname);
+
+    if (urlbits.pathname.toLowerCase() == '/scrape') {
         scrapeHandler(req, res);
     } else {
         console.log('No handler');
-
-        res.writeHead(404, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ 'info': { 'version': '2', 'error': 'Unknown URL' } }, null, 2));
+        
+        await handleScrapeError(res, 'Unknown URL', '', 404);
     }
 };
 
